d89b2c835437a9b079400ce5b40a55cd9cee17a7
184938 <magnusmisje05@gmail.com>
2026-03-12 15:10:30 +0100

    Hand

--- pokerV1/src/Hand.java
+++ pokerV1/src/Hand.java
@@ -1,2 +1,29 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Hand {
+
+    private List<Card> cards;
+
+    public Hand(List<Card> cards) {
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void emptyHand() {
+        cards.clear();
+    }
+
+    public boolean isEmpty() {
+        if (!cards.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
 }
