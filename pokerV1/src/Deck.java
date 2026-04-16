1fb17f69a5c3b452b581efcd07fe9e65337f9f2c
b184896 <bredetorgersen@gmail.com>
2026-04-16 13:16:37 +0200

    doc

--- pokerV1/src/Deck.java
+++ pokerV1/src/Deck.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
 import java.util.Collections;
-
+/**
+ * Representerer en kortstokk bestående av 52 spillkort.
+ * Kortstokken inneholder alle kombinasjoner av {@code Suit} og {@code Rank}.
+ */
 public class Deck {
     private ArrayList<Card> cards = new ArrayList<Card>();
+    /**
+     * Oppretter en ny kortstokk og fyller den med 52 unike kort.
+     */
 
     public Deck () {
         for(Suit suit : Suit.values()) {
@@ -12,21 +18,24 @@ public class Deck {
             }
         }
     }
-
+    /**
+     * Returnerer listen med kort i kortstokken.
+     *
+     * @return en {@code ArrayList} som inneholder alle kortene
+    */
     public ArrayList<Card> getCards() {
         return cards;
     }
-
-    public void shuffle() {
-        Collections.shuffle(cards);
-    }
-
+    //Trekker (og fjerner) det øverste kortet fra kortstokken.
     public Card dealCard() {
         Card card = cards.getFirst();
         cards.remove(card);
         return card;
     }
-
+    /**
+     * Tømmer kortstokken for alle kort.
+     * Etter kall på denne metoden vil kortstokken være tom.
+     */
     public void reset() {
         cards.clear();
     }
